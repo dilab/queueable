64197543730ed12a24fd9b77d4c70a19d1c8474a
xu ding <thedilab@gmail.com>
2016-11-09 16:48:18 +0800

    finish Worker

--- src/Worker.php
+++ src/Worker.php
@@ -11,6 +11,10 @@ class Worker
      */
     private $queue;
 
+    private $maxTries = 5;
+
+    private $sleepSecs = 5;
+
     /**
      * Worker constructor.
      *
@@ -20,29 +24,46 @@ class Worker
         $this->queue = $queue;
     }
 
-    public function work()
+    public function work($maxTries = 5, $sleepSecs = 5)
     {
         while (true) {
 
-            $job = $this->queue->pop();
-
-            if ($job->attempts() > $this->maxTries()) {
-
-                $job->acknowledge();
-
-            }
-
-            try {
-
-                $job->fire();
-
-            } catch (\Exception $e) {
-
-                $job->release();
-
-            }
+            $this->workOnce($maxTries, $sleepSecs);
 
         }
+    }
+
+    protected function workOnce($maxTries, $sleepSecs)
+    {
+        $job = $this->queue->pop();
+
+        if (null == $job) {
+
+            sleep($sleepSecs);
+
+            return 0;
+        }
+
+        if ($job->attempts() > $maxTries) {
+
+            $job->acknowledge();
+
+            return 1;
+        }
+
+        try {
+
+            $job->fire();
+
+            return 2;
+
+        } catch (\Exception $e) {
+
+            $job->release();
+
+            return 3;
+        }
+
     }
 
 }
---
+++ tests/WorkerTest.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace Dilab\Queueable;
+
+
+use Dilab\Queueable\Contract\JobContract;
+use Dilab\Queueable\Driver\InMemoryDriver;
+use Dilab\Queueable\Job\Payload;
+use PHPUnit\Framework\TestCase;
+
+class WorkerOnce extends Worker
+{
+    public function work($maxTries = 5, $sleepSecs = 5)
+    {
+        return $this->workOnce($maxTries, $sleepSecs);
+    }
+}
+
+class WorkerTest extends TestCase
+{
+    /**
+     * @var WorkerOnce
+     */
+    public $worker;
+
+    /**
+     * @var WorkerTestJob
+     */
+    public $job;
+
+    /**
+     * @var Queue
+     */
+    public $queue;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $queueName = 'test';
+
+        $inMemoryDriver = new InMemoryDriver();
+
+        $this->queue = new Queue($queueName, $inMemoryDriver);
+
+        $this->worker = new WorkerOnce($this->queue);
+
+        $this->job = $this->getMockBuilder(WorkerTestJob::class)->setMethods(['handle'])->getMock();
+    }
+
+    public function testWorkOnce_no_job()
+    {
+        $this->assertSame(0, $this->worker->work(1, 0.1));
+    }
+
+    public function testWorkOnce_max_tries_reached()
+    {
+        $this->queue->push($this->job, new Payload());
+        $this->assertSame(1, $this->worker->work(-1, 0.1));
+    }
+
+    public function testWorkOnce_fire_job()
+    {
+        $this->queue->push($this->job, new Payload());
+        $this->assertSame(2, $this->worker->work(1, 0.1));
+    }
+
+    public function testWorkOnce_throw_exception()
+    {
+        $this->job->expects($this->exactly(1))->method('handle')->will($this->throwException(new \Exception()));
+        $this->queue->push($this->job, new Payload());
+        $this->assertSame(3, $this->worker->work(1, 0.1));
+    }
+
+}
+
+class WorkerTestJob implements JobContract
+{
+    public function handle(Payload $payload)
+    {
+        return;
+    }
+
+}
+
