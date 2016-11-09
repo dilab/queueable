ec4ef162a12c40976de495049013ecb20c5ada64
xu ding <thedilab@gmail.com>
2016-11-09 13:02:29 +0800

    done Payload class

---
+++ tests/Job/InMemoryDriverTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Dilab\Queueable\Job;
+
+
+use Cake\TestSuite\TestCase;
+
+class InMemoryDriverTest extends TestCase
+{
+    public $fixtures = [
+
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testDummy()
+    {
+        $this->markTestSkipped();
+    }
+}
---
+++ tests/QueueTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Dilab\Queueable;
+
+
+use Cake\TestSuite\TestCase;
+
+class QueueTest extends TestCase
+{
+    public $fixtures = [
+
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testDummy()
+    {
+        $this->markTestSkipped();
+    }
+}
