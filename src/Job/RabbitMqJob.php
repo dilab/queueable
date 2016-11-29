60096c45fce3ab60edcd613615b986ae5c119566
xu ding <thedilab@gmail.com>
2016-11-29 22:48:31 +0800

    add RabbitMqJob

--- src/Job/RabbitMqJob.php
+++ src/Job/RabbitMqJob.php
@@ -4,7 +4,53 @@
 namespace Dilab\Queueable\Job;
 
 
-class RabbitMqJob
+
+class RabbitMqJob extends Job
 {
+    public function acknowledge()
+    {
+        $this->queue->getDriver()->delete(
+            $this->queue->getQueueName(),
+            $this->message
+        );
+    }
+
+    public function release()
+    {
+        $this->queue->getDriver()->release(
+            $this->queue->getQueueName(),
+            $this->message
+        );
+    }
+
+    public function userJobInstance()
+    {
+        $body = $this->body();
+
+        if (!is_object($body['userJobInstance'])) {
+            return unserialize($body['userJobInstance']);
+        }
+
+        return $body['userJobInstance'];
+    }
+
+    public function payload()
+    {
+        $body = $this->body();
+
+        if (!is_object($body['payload'])) {
+            return unserialize($body['payload']);
+        }
+
+        return $body['payload'];
+    }
+
+    private function body()
+    {
+        if (!is_array($this->message['body'])) {
+            return json_decode($this->message['body'], true);
+        }
+        return $this->message['body'];
+    }
 
 }
--- tests/Job/RabbitMqJobTest.php
+++ tests/Job/RabbitMqJobTest.php
@@ -4,17 +4,35 @@
 namespace Dilab\Queueable\Job;
 
 
-use Cake\TestSuite\TestCase;
+use Dilab\Queueable\Contract\JobContract;
+use Dilab\Queueable\Driver\RabbitMqDriver;
+use Dilab\Queueable\Queue;
+use PHPUnit\Framework\TestCase;
 
 class RabbitMqJobTest extends TestCase
 {
-    public $fixtures = [
+    /**
+     * @var RabbitMqDriver
+     */
+    public $rabbitMqDriver;
 
-    ];
+    /**
+     * @var Queue
+     */
+    public $queue;
+
+    /**
+     * @var RabbitMqJob
+     */
+    public $rabbitMqJob;
 
     public function setUp()
     {
         parent::setUp();
+
+        $this->rabbitMqDriver = $this->getMockBuilder(RabbitMqDriver::class)->disableOriginalConstructor()->getMock();
+
+        $this->queue = new Queue('email', $this->rabbitMqDriver);
     }
 
     public function tearDown()
@@ -22,8 +40,61 @@ class RabbitMqJobTest extends TestCase
         parent::tearDown();
     }
 
-    public function testDummy()
+    public function testAcknowledge()
     {
-        $this->markTestSkipped();
+        $this->rabbitMqJob = new RabbitMqJob($this->queue, $this->message());
+
+        $this->rabbitMqDriver->expects($this->exactly(1))->method('delete');
+
+        $this->rabbitMqJob->acknowledge();
     }
+
+    public function testRelease()
+    {
+        $this->rabbitMqJob = new RabbitMqJob($this->queue, $this->message());
+
+        $this->rabbitMqDriver->expects($this->exactly(1))->method('release');
+
+        $this->rabbitMqJob->release();
+    }
+
+    public function testUserJobInstance()
+    {
+        $this->rabbitMqJob = new RabbitMqJob($this->queue, $this->message());
+
+        $this->assertInstanceOf(JobContract::class, $this->rabbitMqJob->userJobInstance());
+    }
+
+    public function testPayload()
+    {
+        $this->rabbitMqJob = new RabbitMqJob($this->queue, $this->message());
+
+        $this->assertInstanceOf(Payload::class, $this->rabbitMqJob->payload());
+    }
+
+    private function message($body = array())
+    {
+        $message = [
+            'body' => [
+                'id' => 1,
+                'userJobInstance' => serialize(new SqsJobTestJob()),
+                'payload' => serialize(new Payload(['name' => 'Xu']))
+            ],
+            'deliveryTag' => 'tag',
+        ];
+
+        $message['body'] = array_merge($message['body'], $body);
+
+        return $message;
+    }
+}
+
+
+class RabbitMqJobTestJob implements JobContract
+{
+    public function handle(Payload $payload)
+    {
+        return;
+    }
+
 }
